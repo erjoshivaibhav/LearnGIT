5a9e61a2f5fda5c01a65e23b6999650ca827f2e3
Vaibhav Joshi <erjoshivaibhav@gmail.com>
2021-11-10 14:08:29 +0530

    this is my first commit: Commit 1

---
+++ src/package1/Demo1.java
@@ -0,0 +1,12 @@
+package package1;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("we are in demo 1 class");
+
+	}
+
+}
