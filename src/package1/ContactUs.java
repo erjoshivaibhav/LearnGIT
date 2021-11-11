9704d5a910650d20637e50abc0e48f7432acefad
Vaibhav Joshi <erjoshivaibhav@gmail.com>
2021-11-11 18:02:37 +0530

    Contact us Commit

---
+++ src/package1/ContactUs.java
@@ -0,0 +1,10 @@
+package package1;
+
+public class ContactUs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/package1/SignUpFunctionality.java
+++ src/package1/SignUpFunctionality.java
@@ -4,7 +4,9 @@ public class SignUpFunctionality {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		/**
+		 * This utility is for signup functionality
+		 */
 		System.out.println("Need to implement the signup utility");
 
 	}
