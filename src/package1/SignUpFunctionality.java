b189490246bcc2de523875fb99604d3048a8a385
Vaibhav Joshi <erjoshivaibhav@gmail.com>
2021-11-11 17:51:57 +0530

    Sign up class added(Just an empty class)

---
+++ src/package1/SignUpFunctionality.java
@@ -0,0 +1,12 @@
+package package1;
+
+public class SignUpFunctionality {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Need to implement the signup utility");
+
+	}
+
+}
